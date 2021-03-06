36ca4b6cac58b5de4ba768300a3daf8910bd5b12
csantbr <61211407+csantbr@users.noreply.github.com>
2021-03-06 20:00:24 -0300

    adicionando exercício da semana5

---
+++ semana5/MyHolidays/myholidays/holidays.py
@@ -0,0 +1,31 @@
+from datetime import date, datetime
+
+
+# noinspection PyBroadException
+class MyCalendar:
+    def __init__(self, *args):
+        self.datas = []
+        self.datas = self.add_holiday(*args)
+
+    def add_holiday(self, *args):
+        for param in args:
+            if isinstance(param, date):
+                self.datas.append(param)
+            elif type(param) == str:
+                try:
+                    data = datetime.strptime(param, '%d/%m/%Y').date()
+                    self.datas.append(data)
+                except Exception:
+                    pass
+            self.datas = list(set(self.datas))
+        return self.datas
+
+    def check_holiday(self, data):
+        if isinstance(data, date):
+            return data in self.datas
+        elif isinstance(data, str):
+            try:
+                data = datetime.strptime(data, '%d/%m/%Y').date()
+                return data in self.datas
+            except Exception:
+                return False
